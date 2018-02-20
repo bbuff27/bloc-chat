0d843c660a7010ed3ce31dfb39724f24155f2deb
Bill Buff <bbuff27@gmail.com>
2018-02-19 18:57:30 -0700

    Add componentDidMount()

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -3,7 +3,20 @@ import '../styles/messagelist.css';
 
 class MessageList extends Component{
     constructor(props){
+        super(props);
+        this.state = {
+            messages: []
+        }
 
+        this.messagesRef = this.props.firebase.database().ref("messages");
+    }
+
+    componentDidMount(){
+        this.messagesRef.on("child_added", snapshot =>{
+            const message = snapshot.val();
+            message.key = snapshot.key;
+            this.setState({ messages: this.state.messages.concat( message ) })
+        });
     }
 
     render(){
